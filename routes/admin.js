e6044900d9bc3e0dd8f22341ebef13b9c7430f30
piyushbijarania <piyush.gag6513@gmail.com>
2025-09-06 19:27:49 +0530

    added course updation ep

--- routes/admin.js
+++ routes/admin.js
@@ -63,17 +63,35 @@ adminRouter.post("/course", adminMiddleware, async (req, res) => {
 })
 
 
-adminRouter.put("/course", (req, res) => {
+adminRouter.put("/course", adminMiddleware, async (req, res) => {
+    const adminId = req.userId;
+    const { title, description, imageUrl, price, courseId } = req.body;
+    const course = await courseModel.updateOne({
+        _id: courseId,
+        creatorId: adminId
+    }, {
+        title: title,
+        description: description,
+        imageUrl: imageUrl,
+        price: price
+    })
     res.json({
-        message: "signin endpoint"
+        message: "course updated",
+        courseId: course._id
     })
 })
 
 
-adminRouter.get("/course/bulk", (req, res) => {
+adminRouter.get("/course/bulk", adminMiddleware, async (req, res) => {
+    const adminId = req.userId;
+    const courses = await courseModel.findOne({
+        creatorId: adminId
+    });
     res.json({
-        message: "signin endpoint"
+        message: "course updated",
+        courses
     })
+
 })
 
 module.exports = {
